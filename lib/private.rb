a8d7c0828822bf915213c325e68ffe459ca5eb8e
Joanna Bell <jjbdigital@gmail.com>
2022-10-13 11:03:36 -0500

    commit tree 08bba43d8aaf0a5eec9e6c3c99c70a9d5dbc7d5f

---
+++ lib/private.rb
@@ -0,0 +1,44 @@
+require 'pry'
+
+class Bartender
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def intro
+        "Hello, my name is #{self.name}"
+    end
+
+    def make_drink
+        @cocktail_ingredients = []
+        choose_liquor
+        choose_mixer
+        choose_garnish
+        "Here is your drink. It contains #{@cocktail_ingredients}"
+    end
+        
+    private
+
+    def choose_liquor
+      @cocktail_ingredients << "whiskey"
+    end
+  
+    def choose_mixer
+      @cocktail_ingredients << "vermouth"
+    end
+  
+    def choose_garnish
+      @cocktail_ingredients << "olives"
+    end
+end
+
+binding.pry
